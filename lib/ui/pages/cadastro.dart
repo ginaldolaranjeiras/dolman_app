1de08a02d8048ab95daebdbe0fa0e91db8eb102c
Ginaldo Laranjeiras <ginaldoadv@outlook.com.br>
2020-01-29 15:09:46 -0300

    Pequenas adições cadastro

--- lib/ui/pages/cadastro.dart
+++ lib/ui/pages/cadastro.dart
@@ -5,6 +5,7 @@ class Cadastro extends MaterialPageRoute<Null> {
   : super(builder: (BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
         title: const Text(
           'Cadastro'
         ),
@@ -12,72 +13,110 @@ class Cadastro extends MaterialPageRoute<Null> {
       body: Center(
         child: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.all(20.00),
+            padding: EdgeInsets.all(10.00),
             child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
+              Padding(
+                padding: EdgeInsets.all(5.00),
+                child:TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
 
-                  ),
-                  icon: Icon(
-                    Icons.person
                     ),
-                  hintText: 'Como podemos te chamar?',
-                  labelText: 'Nome',
-                ),
-                onSaved: (String value) {
-                },
-                validator: (String value) {
-                  return value.contains('@') ? 'Do not use the @ char.' : null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-
+                    icon: Icon(
+                      Icons.person
+                      ),
+                    hintText: 'Como podemos te chamar?',
+                    labelText: 'Nome',
                   ),
-                  hintText: 'Seu sobrenome',
-                  labelText: 'Sobrenome',
+                  onSaved: (String value) {
+                  },
+                  validator: (String value) {
+                    return value.contains('@') ? 'Do not use the @ char.' : null;
+                  },
                 ),
-                onSaved: (String value) {
-                },
-                validator: (String value) {
-                  return value.contains('@') ? 'Do not use the @ char.' : null;
-                },
               ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(
-                    Icons.mail
+
+              Padding(
+                padding: EdgeInsets.fromLTRB(45.0, 5.0, 5.0, 5.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+
                     ),
-                  border: OutlineInputBorder(
-
+                    hintText: 'Seu sobrenome',
+                    labelText: 'Sobrenome',
                   ),
-                  hintText: 'Seu melhor E-mail',
-                  labelText: 'Email',
+                  onSaved: (String value) {
+                  },
+                  validator: (String value) {
+                    return value.contains('@') ? 'Do not use the @ char.' : null;
+                  },
                 ),
-                onSaved: (String value) {
-                },
-                validator: (String value) {
-                  return value.contains('@') ? 'Do not use the @ char.' : null;
-                },
               ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  icon: Icon(
-                    Icons.lock
-                    ),
-                  border: OutlineInputBorder(
 
+              Padding(
+                padding: EdgeInsets.all(5.00),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    icon: Icon(
+                      Icons.mail
+                      ),
+                    border: OutlineInputBorder(
+
+                    ),
+                    hintText: 'Seu melhor E-mail',
+                    labelText: 'Email',
                   ),
-                  hintText: 'Crie uma senha forte',
-                  labelText: 'Senha',
+                  onSaved: (String value) {
+                  },
+                  validator: (String value) {
+                    return value.contains('@') ? 'Do not use the @ char.' : null;
+                  },
                 ),
-              )
+              ),
+
+              Padding(
+                padding: EdgeInsets.all(5.00),
+                child:TextField(
+                  keyboardType: TextInputType.numberWithOptions(),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.lock
+                      ),
+                    border: OutlineInputBorder(
+
+                    ),
+                    hintText: 'Crie uma senha forte',
+                    labelText: 'Senha',
+                  ),
+                )
+              ),
+
+              Padding(
+                padding: EdgeInsets.fromLTRB(45.0, 5.0, 5.0, 5.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+
+                    ),
+                    hintText: '000.000.000-00',
+                    labelText: 'CPF',
+                  ),
+                  onSaved: (String value) {
+                  },
+                  validator: (String value) {
+                    return value.contains('@') ? 'Do not use the @ char.' : null;
+                  },
+                ),
+              ),
+
+              
+
+
             ],
             ),
 
