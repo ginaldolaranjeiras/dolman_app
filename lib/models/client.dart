2f5628b48ec61f44e8268224f3e3002a08067cfc
Ginaldo Laranjeiras <ginaldoadv@outlook.com.br>
2020-03-07 23:38:06 -0300

    Remoção de importação desnecessária

--- lib/models/client.dart
+++ lib/models/client.dart
@@ -1,5 +1,5 @@
 //Cria a classe do objeto Cliente herdando da superclasse Pessoa e com o retorno do atributo Pay Method
-import 'package:dolman_app/models/address.dart';
+
 import 'package:dolman_app/models/person.dart';
 
 class Client extends Person{
